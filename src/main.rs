05a7da2317b2942787f369233d20445ecbf5daee
Bryan Swift <mail@bryanwrit.es>
2020-02-09 17:45:38 -0500

    Add documentation to `SqsEmailMessages::next`

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,14 @@ impl SqsEmailMessages {
 impl Iterator for SqsEmailMessages {
     type Item = SqsEmailMessage;
 
+    /// Get the next email message identifier from a list of SQS `Message`s. If the current
+    /// `Message` does not represent an email message identifier skip it and try the next one.
+    ///
+    /// Returns [`None`] when there are no `Message` instances left to try, once [`None`] is
+    /// returned there will be no additional [`Some(SqsEmailMessage)`] forthcoming.
+    ///
+    /// [`None`]: https://doc.rust-lang.org/stable/std/option/enum.Option.html#variant.None
+    /// [`Some(Item)`]: https://doc.rust-lang.org/stable/std/option/enum.Option.html#variant.Some
     fn next(&mut self) -> Option<SqsEmailMessage> {
         if self.messages.is_empty() {
             return None;
