5283376da9abd3ec112706680480d8cc5785e28b
Bryan Swift <bryan@bryanjswift.com>
2020-08-23 12:21:52 -0400

    Switch to `tape` from `jest` for testing
    Leave `@aws-cdk/assert` in `devDependencies`. The `@aws-cdk/assert`
    library is written to be used with jest but nothing in the code (as far
    as I could tell) will fail if `jest` is not provided.

--- infrastructure/cdk/test/cdk.test.ts
+++ infrastructure/cdk/test/cdk.test.ts
@@ -1,4 +1,5 @@
 import {App} from '@aws-cdk/core';
+import test from 'tape';
 import {ScalableEmail} from '../lib/scalable-email-stack';
 
 interface Tag {
@@ -6,17 +7,37 @@ interface Tag {
   Value: string;
 }
 
-test('EmaillSenderStack', () => {
+test('EmaillSenderStack', (t) => {
   const app = new App();
   // WHEN
   const stack = new ScalableEmail(app, 'MyTestStack', {stage: 'test'});
   // THEN
-  expect(stack.stackName).toEqual('MyTestStack');
-  expect(stack.tags.hasTags()).toBe(true);
+  t.is(
+    stack.stackName,
+    'MyTestStack',
+    'Stack name should match name from initialization'
+  );
+  t.true(stack.tags.hasTags(), 'Stack should be assigned tags');
   const tags: Tag[] = stack.tags.renderTags();
-  expect(tags.find((tag) => tag.Key === 'Application')).toEqual({
-    Key: 'Application',
-    Value: 'ScalableEmail',
-  });
-  expect(stack.dependencies).toEqual([]);
+  t.is(
+    tags.find((tag) => tag.Key === 'Application')?.Value,
+    'ScalableEmail',
+    'Should have an Application tag identifying itself.'
+  );
+  t.is(
+    tags.find((tag) => tag.Key === 'Stage')?.Value,
+    'test',
+    'Should have a Stage tag with the passed `stage`'
+  );
+  t.deepEqual(
+    stack.dependencies,
+    [],
+    'Should have an empty list of dependencies'
+  );
+  t.is(
+    stack.node.children.length,
+    2,
+    'Should have child constructs for components'
+  );
+  t.end();
 });
