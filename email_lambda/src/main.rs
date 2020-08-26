a19a44df59be010175de8ca11457911ab90c03bd
Bryan Swift <bryan@bryanjswift.com>
2020-08-25 23:09:19 -0400

    Update handler to expect `SqsEvent`

--- email_lambda/src/main.rs
+++ email_lambda/src/main.rs
@@ -1,7 +1,17 @@
+mod de;
+
+use de::MessageDef;
+use email_shared::queue::EmailPointerMessage;
 use lambda_runtime::{error::HandlerError, lambda, Context};
-use log::error;
+use log::info;
 use serde::{Deserialize, Serialize};
 use simplelog::{Config as LogConfig, LevelFilter, SimpleLogger};
+
+#[derive(Deserialize, Clone)]
+struct SqsEvent {
+    #[serde(rename = "Records")]
+    records: Vec<MessageDef>,
+}
 
 #[derive(Deserialize, Clone)]
 struct CustomEvent {
@@ -19,11 +29,17 @@ fn main() {
     lambda!(my_handler);
 }
 
-fn my_handler(e: CustomEvent, _ctx: Context) -> Result<CustomOutput, HandlerError> {
-    if e.first_name == "" {
-        error!("Empty first name");
+fn my_handler(event: SqsEvent, _ctx: Context) -> Result<CustomOutput, HandlerError> {
+    let mut email_ids = Vec::new();
+    for record in event.records.into_iter() {
+        let record_body = record.body.clone();
+        if let Some(pointer) = EmailPointerMessage::from_message(record.into()) {
+            email_ids.push(pointer.email_id);
+        } else {
+            info!("Could not parse email_id from {:?}", record_body);
+        }
     }
     Ok(CustomOutput {
-        message: format!("Hello, {}!", e.first_name),
+        message: format!("Hello {:?}!", email_ids),
     })
 }
