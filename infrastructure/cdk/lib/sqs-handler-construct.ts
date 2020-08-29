087640bce6e1442a09d8bb2020baa954fbf48bfb
Bryan Swift <bryan@bryanjswift.com>
2020-08-29 12:49:40 -0400

    Add log retention setting to `SqsHandler` function

--- infrastructure/cdk/lib/sqs-handler-construct.ts
+++ infrastructure/cdk/lib/sqs-handler-construct.ts
@@ -7,6 +7,7 @@ import {
   Tracing,
 } from '@aws-cdk/aws-lambda';
 import {SqsEventSource} from '@aws-cdk/aws-lambda-event-sources';
+import {RetentionDays} from '@aws-cdk/aws-logs';
 import {IQueue} from '@aws-cdk/aws-sqs';
 import {existsSync} from 'fs';
 import {join, resolve} from 'path';
@@ -41,6 +42,7 @@ export class SqsHandler extends Construct {
       environment: {
         DYNAMO_TABLE: `email_db_${stage}`,
       },
+      logRetention: RetentionDays.ONE_WEEK,
       role: handlerRole,
       functionName: `email_handler_${stage}`,
       handler: 'doesnt.matter',
