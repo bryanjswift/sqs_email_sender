23388a7b69d6c57efe0c8aea33dff42a1e60bda6
Bryan Swift <bryan@bryanjswift.com>
2020-08-29 22:27:01 -0400

    Add the `n` function to the `DynamoItemWrapper`

--- email_shared/src/attribute_value_wrapper.rs
+++ email_shared/src/attribute_value_wrapper.rs
@@ -51,12 +51,94 @@ impl DynamoItemWrapper {
     /// use std::collections::HashMap;
     /// use email_shared::attribute_value_wrapper::DynamoItemWrapper;
     ///
+    /// let mut item: HashMap<String, AttributeValue> = HashMap::new();
+    /// item.insert(
+    ///     "foo".into(),
+    ///     AttributeValue {
+    ///         s: Some("My String".into()),
+    ///         ..AttributeValue::default()
+    ///     }
+    /// );
+    /// let wrapper = DynamoItemWrapper::new(item);
+    /// assert!(wrapper.s("foo", "bar") == Ok("My String".into()));
+    /// ```
+    ///
+    /// ```
+    /// use rusoto_dynamodb::AttributeValue;
+    /// use std::collections::HashMap;
+    /// use email_shared::attribute_value_wrapper::DynamoItemWrapper;
+    ///
     /// let item: HashMap<String, AttributeValue> = HashMap::new();
+    /// let wrapper = DynamoItemWrapper::new(item);
+    /// assert!(wrapper.s("foo", "bar") == Err("bar"));
+    /// ```
+    ///
+    /// ```
+    /// use rusoto_dynamodb::AttributeValue;
+    /// use std::collections::HashMap;
+    /// use email_shared::attribute_value_wrapper::DynamoItemWrapper;
+    ///
+    /// let mut item: HashMap<String, AttributeValue> = HashMap::new();
+    /// item.insert("foo".into(), AttributeValue::default());
     /// let wrapper = DynamoItemWrapper::new(item);
     /// assert!(wrapper.s("foo", "bar") == Err("bar"));
     /// ```
     pub fn s<E>(&self, key: &str, error: E) -> Result<String, E> {
         self.item.get(key).and_then(|av| av.s.clone()).ok_or(error)
+    }
+
+    /// Try to retrieve an `AttributeValue` for `key` and then try to get the number value from the
+    /// associated `AttributeValue`. If either retrieving an `AttributeValue` or getting a number
+    /// value fails wrap the given `error` in `Err`. Because Dynamo DB transmits number values as
+    /// strings the `Result` holds a `String` value.
+    ///
+    /// # Examples
+    ///
+    /// ## Get a numeric value
+    ///
+    /// ```
+    /// use rusoto_dynamodb::AttributeValue;
+    /// use std::collections::HashMap;
+    /// use email_shared::attribute_value_wrapper::DynamoItemWrapper;
+    ///
+    /// let mut item: HashMap<String, AttributeValue> = HashMap::new();
+    /// item.insert(
+    ///     "foo".into(),
+    ///     AttributeValue {
+    ///         n: Some("123.45".into()),
+    ///         ..AttributeValue::default()
+    ///     }
+    /// );
+    /// let wrapper = DynamoItemWrapper::new(item);
+    /// assert!(wrapper.n("foo", "bar") == Ok("123.45".into()));
+    /// ```
+    ///
+    /// ## `Err` for non-existant attribute
+    ///
+    /// ```
+    /// use rusoto_dynamodb::AttributeValue;
+    /// use std::collections::HashMap;
+    /// use email_shared::attribute_value_wrapper::DynamoItemWrapper;
+    ///
+    /// let item: HashMap<String, AttributeValue> = HashMap::new();
+    /// let wrapper = DynamoItemWrapper::new(item);
+    /// assert!(wrapper.n("foo", "bar") == Err("bar"));
+    /// ```
+    ///
+    /// ## `Err` for empty number
+    ///
+    /// ```
+    /// use rusoto_dynamodb::AttributeValue;
+    /// use std::collections::HashMap;
+    /// use email_shared::attribute_value_wrapper::DynamoItemWrapper;
+    ///
+    /// let mut item: HashMap<String, AttributeValue> = HashMap::new();
+    /// item.insert("foo".into(), AttributeValue::default());
+    /// let wrapper = DynamoItemWrapper::new(item);
+    /// assert!(wrapper.n("foo", "bar") == Err("bar"));
+    /// ```
+    pub fn n<E>(&self, key: &str, error: E) -> Result<String, E> {
+        self.item.get(key).and_then(|av| av.n.clone()).ok_or(error)
     }
 }
 
@@ -69,7 +151,30 @@ mod s {
     const EMAIL_ID_KEY: &str = "EmailId";
     const EMAIL_ID_VALUE: &str = "foo";
 
-    fn create_item() -> HashMap<String, AttributeValue> {
+    #[test]
+    fn error_when_missing() {
+        let attributes = HashMap::new();
+        let wrapper = DynamoItemWrapper::new(attributes);
+        assert_eq!(wrapper.s(EMAIL_ID_VALUE, ERROR_MSG), Err(ERROR_MSG));
+    }
+
+    #[test]
+    fn error_when_wrong_type() {
+        // Attribute has the correct value but under the wrong type
+        let mut attributes = HashMap::new();
+        attributes.insert(
+            EMAIL_ID_KEY.into(),
+            AttributeValue {
+                n: Some(EMAIL_ID_VALUE.into()),
+                ..AttributeValue::default()
+            },
+        );
+        let wrapper = DynamoItemWrapper::new(attributes);
+        assert_eq!(wrapper.s(EMAIL_ID_KEY, ERROR_MSG), Err(ERROR_MSG));
+    }
+
+    #[test]
+    fn ok_when_exists() {
         let mut attributes = HashMap::new();
         attributes.insert(
             EMAIL_ID_KEY.into(),
@@ -78,20 +183,58 @@ mod s {
                 ..AttributeValue::default()
             },
         );
-        attributes
+        let wrapper = DynamoItemWrapper::new(attributes);
+        assert_eq!(
+            wrapper.s(EMAIL_ID_KEY, ERROR_MSG),
+            Ok(EMAIL_ID_VALUE.into())
+        );
     }
+}
+
+#[cfg(test)]
+mod n {
+    use super::*;
+    use std::collections::HashMap;
+
+    const ERROR_MSG: &str = "error";
+    const EMAIL_ID_KEY: &str = "EmailId";
+    const EMAIL_ID_VALUE: &str = "123.45";
 
     #[test]
     fn error_when_missing() {
-        let wrapper = DynamoItemWrapper::new(create_item());
-        assert_eq!(wrapper.s(EMAIL_ID_VALUE, ERROR_MSG), Err(ERROR_MSG));
+        let attributes = HashMap::new();
+        let wrapper = DynamoItemWrapper::new(attributes);
+        assert_eq!(wrapper.n(EMAIL_ID_KEY, ERROR_MSG), Err(ERROR_MSG));
+    }
+
+    #[test]
+    fn error_when_wrong_type() {
+        // Attribute has the correct value but under the wrong type
+        let mut attributes = HashMap::new();
+        attributes.insert(
+            EMAIL_ID_KEY.into(),
+            AttributeValue {
+                s: Some(EMAIL_ID_VALUE.into()),
+                ..AttributeValue::default()
+            },
+        );
+        let wrapper = DynamoItemWrapper::new(attributes);
+        assert_eq!(wrapper.n(EMAIL_ID_KEY, ERROR_MSG), Err(ERROR_MSG));
     }
 
     #[test]
     fn ok_when_exists() {
-        let wrapper = DynamoItemWrapper::new(create_item());
+        let mut attributes = HashMap::new();
+        attributes.insert(
+            EMAIL_ID_KEY.into(),
+            AttributeValue {
+                n: Some(EMAIL_ID_VALUE.into()),
+                ..AttributeValue::default()
+            },
+        );
+        let wrapper = DynamoItemWrapper::new(attributes);
         assert_eq!(
-            wrapper.s(EMAIL_ID_KEY, ERROR_MSG),
+            wrapper.n(EMAIL_ID_KEY, ERROR_MSG),
             Ok(EMAIL_ID_VALUE.into())
         );
     }
