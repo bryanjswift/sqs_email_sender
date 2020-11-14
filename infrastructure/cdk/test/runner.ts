67fe141d3af05cf66997970471d80b172fc6e0b3
Bryan Swift <bryan@bryanjswift.com>
2020-11-13 22:24:25 -0500

    Change `verbosity` to `VerbosityLevel` instead of boolean
    Use `readonly` arrays as parameters, assign array values rather than
    mutating the array.

--- infrastructure/cdk/test/runner.ts
+++ infrastructure/cdk/test/runner.ts
@@ -1,16 +1,16 @@
 import test from 'tape';
 import {format} from 'util';
 
-let verbose = process.env.TAPE_VERBOSE === 'true';
+const verbosity: VerbosityLevel = getVerbosity(process.env.TAPE_VERBOSITY);
 
 setupReporter();
 
 /**
  * Creates a readable stream from `tape` and manages writing results to stdout.
  */
-function setupReporter() {
-  let context: string[] = [];
-  let failures: FailureWithContext[] = [];
+function setupReporter(): void {
+  let context: readonly string[] = [];
+  let failures: readonly FailureWithContext[] = [];
   let results: ResultSummary = {
     failCount: 0,
     okCount: 0,
@@ -18,7 +18,7 @@ function setupReporter() {
 
   const stream = test.createStream({objectMode: true});
   stream.on('data', (chunk: unknown) => {
-    if (verbose && isCommentLine(chunk)) {
+    if (verbosity === 'full' && isCommentLine(chunk)) {
       process.stdout.write(commentLineReport(context, chunk));
       return;
     } else if (
@@ -30,7 +30,7 @@ function setupReporter() {
 
     switch (chunk.type) {
       case 'test':
-        context.push(chunk.name);
+        context = context.concat(chunk.name);
         break;
       case 'assert':
         process.stdout.write(assertionLineReport(context, chunk));
@@ -43,7 +43,7 @@ function setupReporter() {
         };
         break;
       case 'end':
-        context.pop();
+        context = context.slice(0, -1);
         break;
     }
   });
@@ -59,14 +59,19 @@ function setupReporter() {
  * @param chunk with data about the assertion/test
  * @returns the formatted assertion string
  */
-function assertionLineReport(context: string[], chunk: Assert): string {
-  if (verbose) {
+function assertionLineReport(
+  context: readonly string[],
+  chunk: Assert
+): string {
+  if (verbosity === 'full') {
     const result = chunk.ok ? 'ok' : 'not ok';
     const suffix = chunk.name;
     return format('%s: %s - %s\n', result, makeContext(context), suffix);
-  } else {
+  } else if (verbosity === 'lite') {
     // Append a dot?
     return '.';
+  } else {
+    return '';
   }
 }
 
@@ -76,8 +81,11 @@ function assertionLineReport(context: string[], chunk: Assert): string {
  * @param comment to be reported
  * @returns the formatted comment string
  */
-function commentLineReport(context: string[], comment: Comment): string {
-  if (verbose) {
+function commentLineReport(
+  context: readonly string[],
+  comment: Comment
+): string {
+  if (verbosity === 'full') {
     return format('# %s %s\n', makeContext(context, ' -'), comment);
   } else {
     return '';
@@ -90,9 +98,9 @@ function commentLineReport(context: string[], comment: Comment): string {
  * @param failures containing the test failures to report upon
  * @returns the formatted string
  */
-function failureSummaryReport(failures: FailureWithContext[]): string {
+function failureSummaryReport(failures: readonly FailureWithContext[]): string {
   let out = failures.length === 0 ? '' : '\n';
-  if (verbose && failures.length > 0) {
+  if ((verbosity === 'full' || verbosity === 'lite') && failures.length > 0) {
     // Print an empty line
     out += '** Test Failure Summary **\n';
     out += '\n';
@@ -114,34 +122,82 @@ function failureSummaryReport(failures: FailureWithContext[]): string {
  * @returns the formatted string
  */
 function resultsSummaryReport(results: ResultSummary): string {
-  let out = verbose ? '\n' : '\n\n';
+  let out = verbosity === 'summary' ? '' : verbosity === 'full' ? '\n' : '\n\n';
   out += `not ok: ${results.failCount}\n`;
   out += `ok: ${results.okCount}\n`;
   return out;
 }
 
+/**
+ * Assume the given value is a given "object" type with string keys if it is an
+ * object.
+ * @param x to be assumed
+ * @returns `true` if `x` exists and is not `null`
+ */
 function isAssumedType<T extends Record<string, unknown>>(x: unknown): x is T {
   return typeof x === 'object' && x !== null;
 }
 
+/**
+ * Check if the given `line` is of the `Assert` type.
+ * @param line to be checked
+ * @returns `true` if `line` has the `'assert'` type value
+ */
 function isAssertLine(line: unknown): line is Assert {
   return isAssumedType(line) && 'type' in line && line.type === 'assert';
 }
 
+/**
+ * Check if the given `line` is of the `Comment` type
+ * @param line to be checked
+ * @returns `true` if `line` is a comment.
+ */
 function isCommentLine(line: unknown): line is Comment {
   return typeof line === 'string';
 }
 
+/**
+ * Check if the given `line` is of the `End` type
+ * @param line to be checked
+ * @returns `true` if `line` has the `'end'` type value.
+ */
 function isEndLine(line: unknown): line is End {
   return isAssumedType(line) && 'type' in line && line.type === 'end';
 }
 
+/**
+ * Check if the given `line` is of the `Test` type.
+ * @param line to be checked
+ * @returns `true` if `line` has the `'test'` type value
+ */
 function isTestLine(line: unknown): line is Test {
   return isAssumedType(line) && 'type' in line && line.type === 'test';
 }
 
-function makeContext(context: string[], suffix = ''): string {
+/**
+ * Create a context string from an a context "stack".
+ * @param context stack to include in the string
+ * @param suffix to append to the context
+ * @returns a combination of context and `suffix`
+ */
+function makeContext(context: readonly string[], suffix = ''): string {
   return `${context.join(' - ')}${suffix}`;
+}
+
+/**
+ * Get the `VerbosityLevel` based on the given `string` value.
+ * @param verbosity to turn into `VerbosityLevel`
+ * @returns the level
+ */
+function getVerbosity(verbosity?: string): VerbosityLevel {
+  switch (verbosity) {
+    case 'full': // Intentional fall through
+    case 'lite': // Intentional fall through
+    case 'summary':
+      return verbosity;
+    default:
+      return 'lite';
+  }
 }
 
 type Assert = AssertSuccess | AssertFailure;
@@ -200,3 +256,5 @@ interface WithContext {
 }
 
 type Comment = string;
+
+type VerbosityLevel = 'full' | 'lite' | 'summary';
