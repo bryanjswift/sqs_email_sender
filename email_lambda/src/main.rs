217e07da2e36a4040dffc104d86d062a02ea3ec5
Bryan Swift <bryan@bryanjswift.com>
2021-03-14 11:59:00 -0400

    Use `tracing-futures` in `email_lambda`
    The `tracing-futures` crate makes it easier to manage entering and
    exiting spans across async/await boundaries.

--- email_lambda/src/main.rs
+++ email_lambda/src/main.rs
@@ -13,6 +13,7 @@ use rusoto_sqs::{DeleteMessageBatchRequest, Sqs, SqsClient};
 use serde::{Deserialize, Serialize};
 use std::env;
 use tracing::{event, span, Level};
+use tracing_futures::Instrument;
 
 const DYNAMO_TABLE: &str = "DYNAMO_TABLE";
 const QUEUE_URL: &str = "QUEUE_URL";
@@ -68,6 +69,7 @@ async fn handler(
     // Process each event record
     let entries_to_delete = client
         .process_messages(event.records.into_iter().map(|record| record.into()))
+        .in_current_span()
         .await;
     // Compare the number of messages to be deleted with the number received
     let entries_to_delete_count = entries_to_delete.len();
@@ -84,6 +86,7 @@ async fn handler(
                 entries: entries_to_delete,
                 queue_url,
             })
+            .instrument(tracing::info_span!("delete_message_batch"))
             .await;
         let error = match delete_response {
             Ok(_) if entries_to_delete_count > 0 => EmailHandlerError::PartialBatchFailure,
